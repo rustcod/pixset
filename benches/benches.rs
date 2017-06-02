ba923eab63a18aebb21d06bc861d863d97325fe4
Dave O <davidobrite@gmail.com>
2017-06-02 08:54:26 -0700

    chore(benches): add bench for pixset get

--- benches/benches.rs
+++ benches/benches.rs
@@ -9,3 +9,9 @@ use test::Bencher;
 fn bench_font_iter(b: &mut Bencher) {
     b.iter(|| for (_pix, _offset) in pixset::Str::from("Yo, Dawg;").iter() {},)
 }
+
+#[bench]
+fn bench_pixset_get(b: &mut Bencher) {
+    let pixset = pixset::Pixset::new(100, 16);
+    b.iter(|| pixset.get(&pixset::Pix::A))
+}
